4edd3cde3e1f261367a168f666facea81df6904f
Mattia Lanzi <mlanzi@chicagobooth.edu>
2023-04-12 20:59:31 +0000

    Only missing payment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,17 +4,31 @@ def blank_square_form
 render({ :template => "calculation_templates/square_form.html.erb"})
   end 
 
-  def calculate_square
-    @num = params.fetch("elephant").to_f
-    @square_of_num = @num ** 2
-    render({ :template => "calculation_templates/square_results.html.erb"})
-      end 
+def calculate_square
+  @num = params.fetch("elephant").to_f
+  @square_of_num = @num ** 2
+  render({ :template => "calculation_templates/square_results.html.erb"})
+    end 
 
-  def calculate_random
-    @lower = params.fetch("user_min").to_f
-    @upper = params.fetch("user_max").to_f
-    @result = rand(@lower..@upper)
-    render({ :template => "calculation_templates/rand_results.html.erb"})
-      end 
+def blank_random_form
+  render({ :template => "calculation_templates/random_form.html.erb"})
+    end 
+
+def calculate_random
+  @lower = params.fetch("user_min").to_f
+  @upper = params.fetch("user_max").to_f
+  @result = rand(@lower..@upper)
+  render({ :template => "calculation_templates/rand_results.html.erb"})
+    end 
+
+def blank_square_root_form
+  render({ :template => "calculation_templates/square_root_form.html.erb"})
+    end 
+    
+def calculate_square_root
+  @nume = params.fetch("user_number").to_f
+  @square_root_of_num = @nume ** 0.5
+  render({ :template => "calculation_templates/square_root_results.html.erb"})
+     end
 
 end
