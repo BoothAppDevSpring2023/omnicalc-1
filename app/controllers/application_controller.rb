2236f589221375d83e9f951f1fa881f1994d0bbf
Mattia Lanzi <mlanzi@chicagobooth.edu>
2023-04-13 01:15:59 +0000

    Final version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,24 +36,14 @@ def blank_payment_form
     end 
         
 def calculate_payment
-  rate = params["number1"].to_f
-  years = params["number2"].to_f
-  principal = params["number3"].to_f
-  payment = ((rate / 1200 * principal) / (1-((1 + rate / 1200) ** (-years * 12))))
-  msg1 = "Payment results"
-  no_imput = 5
-  btn_text = "Calculate another payment"
-  lbl_1 = "APR"
-  lbl_2 = "Number of years"
-  lbl_3 = "Principal"
-  plh_1 = "Payment"
-  plh_2 = rate.round(1).to_s
-  plh_3 = years
-  act = "$" + principal.to_s
-  type = "$" + payment.round(2).to_s
-  opt_url = "payment/new"
-  render({ :template => "calculation_templates/payment_results.html.erb"}, :locals => { :msg1 => msg1, :no_imput => no_imput, :btn_text => btn_text, :lbl_1 => lbl_1, :lbl_2 => lbl_2, :lbl_3 => lbl_3, :plh_1 => plh_1, :plh_2 => plh_2, :plh_3 => plh_3, :act => act, :type => type, :opt_url => opt_url})
-    end
+  @rate = params["user_apr"].to_f
+  @years = params["user_years"].to_f
+  @principal = params["user_pv"].to_f
+  @payment = ((@rate / 1200 * @principal) / (1-((1 + @rate / 1200) ** (-@years * 12)))).round(2)
+  @opt_url = "/payment/new"
 
+  render({ :template => "calculation_templates/payment_results.html.erb"})
+  end
+   
 
 end
